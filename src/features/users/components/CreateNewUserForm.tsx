91f73314a081a2b453d59234f9e8528d07a35fc9
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2024-06-09 11:20:54 +0300

    fix create users error

--- src/features/users/components/CreateNewUserForm.tsx
+++ src/features/users/components/CreateNewUserForm.tsx
@@ -8,172 +8,169 @@ import { ToastContainer, toast } from "react-toastify"
 
 const CreateNewUserForm = () => {
 
-	const navigation = useNavigate()
+  const navigation = useNavigate()
 
-	const [formData, setFormData] = useState({
-		username: '',
-		email: '',
-		job_title: '',
-		national_id: '',
-		password: '',
-		is_admin: ''
-	})
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    job_title: '',
+    national_id: '',
+    password: '',
+    is_admin: ''
+  })
 
-	console.log(formData.is_admin)
-	console.log(formData.job_title)
+  const postUser = useMutation({
+    mutationFn: () => {
+      return PostNewUser(formData)
+    },
+    onSuccess: () => {
+      navigation(pathList.homePage)
+      toast.success('تم اضافة مستخدم جديد بنجاح')
+    },
+    onError: (error) => {
+      // @ts-ignore
+      toast.error(error?.response?.data?.message)
+    }
+  })
 
-	const postUser = useMutation({
-		mutationFn: () => {
-			return PostNewUser(formData)
-		},
-		onSuccess: () => {
-			navigation(pathList.homePage)
-			toast.success('تم اضافة مستخدم جديد بنجاح')
-		},
-		onError: (error) => {
-			// @ts-ignore
-			toast.error(error?.response?.data?.message)
-		}
-	})
+  const onSubmit = (e: any) => {
+    e.preventDefault()
+    postUser.mutate()
+  }
+  return (
+    <>
+      <ToastContainer />
+      <form onSubmit={onSubmit}>
+        <div className="row">
+          <div className="col-12">
+            <div className="form-heading">
+              <h4>اضافة مستخدم جديد</h4>
+            </div>
+          </div>
+          <FormInput
+            label="اسم المستخدم"
+            required
+            type="text"
+            name="username"
+            onChange={
+              (text) => {
+                setFormData((prev) => {
+                  return {
+                    ...prev,
+                    username: text
+                  }
+                })
+              }
+            }
+          />
+          <FormInput
+            label="البريد الالكتروني"
+            required
+            type="email"
+            name="email"
+            onChange={
+              (text) => {
+                setFormData((prev) => {
+                  return {
+                    ...prev,
+                    email: text
+                  }
+                })
+              }
+            }
+          />
+          <FormInput
+            label="المسمي الوظيفي"
+            required
+            type="text"
+            name="job_title"
+            onChange={
+              (text) => {
+                setFormData((prev) => {
+                  return {
+                    ...prev,
+                    job_title: text
+                  }
+                })
+              }
+            }
+          />
+          <FormInput
+            label="رقم الهوية الوطنية"
+            required
+            type="number"
+            name="national_id"
+            onChange={
+              (text) => {
+                setFormData((prev) => {
+                  return {
+                    ...prev,
+                    national_id: text
+                  }
+                })
+              }
+            }
+          />
+          <FormInput
+            label="الرقم السري"
+            required
+            type="text"
+            name="password"
+            onChange={
+              (text) => {
+                setFormData((prev) => {
+                  return {
+                    ...prev,
+                    password: text
+                  }
+                })
+              }
+            }
+          />
+          <div className="col-12 col-md-6 col-xl-6">
+            <div className="select-gender">
+              <label className="gen-label">
+                هل تريد اعطاء المستخدم صلاحيات الادمن <span className="login-danger">*</span>
+              </label>
+              <select
+                name="is_admin"
+                id="is_admin"
+                className=" w-100 mb-5"
+                onChange={(text) => {
+                  setFormData((prev: any) => {
+                    return {
+                      ...prev,
+                      is_admin: JSON.parse(text.target.value)
+                    }
+                  })
+                }}
+              >
+                <option value={JSON.parse(`${false}`)}>لا</option>
+                <option value={JSON.parse(`${true}`)}>نعم</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="col-12">
+          <div className="doctor-submit">
+            <button
+              type="submit"
+              className="btn btn-primary submit-form me-2"
+            >
+              إضافة مستخدم جديد
+            </button>
+            <Link
+              to={pathList.homePage}
+              className="btn btn-primary cancel-form"
+            >
+              رجوع
+            </Link>
+          </div>
 
-	const onSubmit = (e: any) => {
-		e.preventDefault()
-		postUser.mutate()
-	}
-	return (
-		<>
-			<ToastContainer />
-			<form onSubmit={onSubmit}>
-				<div className="row">
-					<div className="col-12">
-						<div className="form-heading">
-							<h4>اضافة مستخدم جديد</h4>
-						</div>
-					</div>
-					<FormInput
-						label="اسم المستخدم"
-						required
-						type="text"
-						name="username"
-						onChange={
-							(text) => {
-								setFormData((prev) => {
-									return {
-										...prev,
-										username: text
-									}
-								})
-							}
-						}
-					/>
-					<FormInput
-						label="البريد الالكتروني"
-						required
-						type="email"
-						name="email"
-						onChange={
-							(text) => {
-								setFormData((prev) => {
-									return {
-										...prev,
-										email: text
-									}
-								})
-							}
-						}
-					/>
-					<FormInput
-						label="المسمي الوظيفي"
-						required
-						type="text"
-						name="job_title"
-						onChange={
-							(text) => {
-								setFormData((prev) => {
-									return {
-										...prev,
-										job_title: text
-									}
-								})
-							}
-						}
-					/>
-					<FormInput
-						label="رقم الهوية الوطنية"
-						required
-						type="number"
-						name="national_id"
-						onChange={
-							(text) => {
-								setFormData((prev) => {
-									return {
-										...prev,
-										national_id: text
-									}
-								})
-							}
-						}
-					/>
-					<FormInput
-						label="الرقم السري"
-						required
-						type="text"
-						name="password"
-						onChange={
-							(text) => {
-								setFormData((prev) => {
-									return {
-										...prev,
-										password: text
-									}
-								})
-							}
-						}
-					/>
-					<div className="col-12 col-md-6 col-xl-6">
-						<div className="select-gender">
-							<label className="gen-label">
-								هل تريد اعطاء المستخدم صلاحيات الادمن <span className="login-danger">*</span>
-							</label>
-							<select
-								name="is_admin"
-								id="is_admin"
-								className=" w-100 mb-5"
-								onChange={(text) => {
-									setFormData((prev: any) => {
-										return {
-											...prev,
-											is_admin: text.target.value
-										}
-									})
-								}}
-							>
-								<option value={`${false}`}>لا</option>
-								<option value={`${true}`}>نعم</option>
-							</select>
-						</div>
-					</div>
-				</div>
-				<div className="col-12">
-					<div className="doctor-submit">
-						<button
-							type="submit"
-							className="btn btn-primary submit-form me-2"
-						>
-							إضافة مستخدم جديد
-						</button>
-						<Link
-							to={pathList.homePage}
-							className="btn btn-primary cancel-form"
-						>
-							رجوع
-						</Link>
-					</div>
-
-				</div>
-			</form>
-		</>
-	)
+        </div>
+      </form>
+    </>
+  )
 }
 
 export default CreateNewUserForm
