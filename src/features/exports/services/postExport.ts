16b3608e9d7b26009e388de992d8a4e3dd09907c
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2024-06-13 09:08:35 +0300

    Update postExport.ts

--- src/features/exports/services/postExport.ts
+++ src/features/exports/services/postExport.ts
@@ -3,7 +3,7 @@ import axiosInstance from "../../../lib/axiosInstance"
 const postExport = (data: any) => {
     const formData = new FormData()
     formData.append('receiver_name', data?.receiver_name)
-    formData.append('orders', data?.orders)
+    formData.append('orders', JSON.stringify(data?.orders))
     formData.append('date', data?.date)
     formData.append('invoice_date', data?.invoice_date)
     formData.append('attachment', data?.attachment)
